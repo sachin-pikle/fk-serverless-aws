e46ff1275e0d78d96ba3fadf1098b160795ef5a1
sachin-pikle <spikle.dev@gmail.com>
2018-12-28 23:32:30 +0530

    v11

--- js/apiGateway-js-sdk/apigClient.js
+++ js/apiGateway-js-sdk/apigClient.js
@@ -101,6 +101,24 @@ apigClientFactory.newClient = function (config) {
     };
     
     
+    apigClient.conversationsPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
+        
+        var conversationsPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/conversations').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(conversationsPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
     apigClient.conversationsOptions = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
         
